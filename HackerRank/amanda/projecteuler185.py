e2dcb14590cebd7471e35c33ba2e479e1b35fa69
Amanda F <amandathais1458@gmail.com>
2020-08-22 23:09:06 -0300

    atualize euler185

--- HackerRank/amanda/projecteuler185.py
+++ HackerRank/amanda/projecteuler185.py
@@ -4,6 +4,7 @@ n = int(input())
 m = []
 mc = []
 chars = 12
+certas =[]
 for i in range(chars):
     m.append(['1','2','3','4','5','6','7','8','9','0'])
     mc.append(['0','0','0','0','0','0','0','0','0','0'])
@@ -20,8 +21,12 @@ for i in range(n):
             if num[j] in m[j]:
                 m[j].remove(num[j])
     else:
+        certas.append(num)
         for j in range(chars):
             if num[j] in m[j]:
-                mc[j][int(num[j])-1] = num[j]
+                mc[j][int(num[j])-1] = int(mc[j][int(num[j])-1]) + int(num[j])
+##    for j in range(len(certas)):
+##        if num[j] in
 print(m)
 print(mc)
+print(certas)
